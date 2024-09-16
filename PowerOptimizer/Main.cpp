13ca91d6ffdce7822d0e0392241dab5c78f5a7a2
Kristian <Kristian@KRISTIAN>
2024-09-16 20:12:57 +0200

    Add project files.

---
+++ PowerOptimizer/Main.cpp
@@ -0,0 +1,13 @@
+#include "Window.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	PowerOptimizer::Window w;
+	Application::Run(%w);
+}

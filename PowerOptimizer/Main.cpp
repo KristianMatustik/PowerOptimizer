5ef726bd5c5450c81b4581691f7dfb7f79f97226
Kristian <kristianmatustik@gmail.com>
2025-01-10 13:52:23 +0100

    minor fixes, added aditional files for thesis

--- PowerOptimizer/Main.cpp
+++ PowerOptimizer/Main.cpp
@@ -11,26 +11,26 @@ using namespace System::Windows::Forms;
 [STAThreadAttribute]
 int main(array<String^>^ args)
 {
-	Track t;
-	Cyclist c(92, 0.28, 0.4, 500, 0.97, 2, 30, 2.5);
+	//Track t;
+	//Cyclist c(92, 0.28, 0.4, 500, 0.97, 2, 30, 2.5);
 	//Cyclist c(90, 0.22, 0.22, 5000, 0.97, 2, 30, 2.5);
-	Tester tst(t, c);
-	Optimizer o(&t, &c);
+	//Tester tst(t, c);
+	//Optimizer o(&t, &c);
 
 	//t.load_GPX("gpx/test.gpx");
 	//t.set_corners(c);
 
 	
-	tst.setHill(1000, 0.1, 500);
+	//tst.setHill(1000, 0.1, 500);
 	//tst.setA(1000, 0.1);
 	//tst.setFlat(500);
 	//tst.setWind(1000, -5);
 
-	o.solve_AP(360, 600,5);
+	//o.solve_AP(360, 600,5);
 	//t.save_GPX("gpx/0.gpx");
 
 	//o.solve_CP(300,10000,600,-1,3);
-	t.save_GPX("gpx/0.gpx");
+	//t.save_GPX("gpx/0.gpx");
 	//t.save_GPX("gpx/CP_real.gpx");
 
 	//Track t2;
